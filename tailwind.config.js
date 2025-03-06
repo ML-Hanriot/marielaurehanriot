42ece8dd25981577e2776b522a19cefa05281e71
Marie-LH <marielaurehanriot@gmail.com>
2025-03-06 15:03:03 +0100

    MAJ

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+// 	theme: {
+// 		extend: {
+// 			fontSize: {
+// 				h1: "3rem",
+// 				h2: "2.5rem",
+// 				h3: "2rem",
+// 			},
+// 		},
+// 	},
+// };
+module.exports = {
+	mode: "jit", // JIT activé
+	content: [
+		"./path/to/your/files/**/*.html",
+		"./path/to/your/files/**/*.js",
+		"./path/to/your/files/**/*.twig",
+	],
+	// Autres configurations...
+};
