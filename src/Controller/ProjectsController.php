1b40ee1a1bb7cdb2563df77784ed0687145733dd
Marie-LH <marielaurehanriot@gmail.com>
2025-03-17 19:39:33 +0100

    Ajout route EntreQueers

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/projects')]
 final class ProjectsController extends AbstractController{
-    #[Route('/projects', name: 'app_projects')]
-    public function index(): Response
+    #[Route('', name: 'app_projects')]
+    public function projects(): Response
     {
         return $this->render('projects/index.html.twig', [
+            'controller_name' => 'ProjectsController',
+        ]);
+    }
+    #[Route('/entre_queers', name: 'app_projects/entre_queers')]
+    public function show_entre_queers(): Response
+    {
+        return $this->render('projects/entre_queers.html.twig', [
             'controller_name' => 'ProjectsController',
         ]);
     }
